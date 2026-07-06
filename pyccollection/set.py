47df3e59f2625ef28187e578476f75e469660d95
Festus <ojofestus2030@gmail.com>
2026-07-06 11:57:49 +0100

    Add initial implementation of set operations and examples

---
+++ pyccollection/set.py
@@ -0,0 +1,36 @@
+"""
+set
+"""
+# Set and set operation
+# set is unorder collectiom of unique element
+# set1 ={}
+
+# set2 = set()
+# set3 = set(())
+# set4 = set([])
+
+# student_name = {"Oba", "Festus", "Emmanuel", "Kenny", "Ibrahim", "Samuel", "Festus"}
+# student_name2 = {"Fatimah", "Gafar", "Samuel"}
+
+# print (student_name[1])
+# print (student_name)
+# print (len(student_name))
+
+# print(student_name.union(student_name2))
+# print(student_name | student_name2)
+# print(student_name2.intersection(student_name))
+# print(student_name2.difference(student_name))
+# print(student_name2 - student_name)
+
+# print(student_name.add("Mango"))
+# print(student_name)
+
+# set1 = {1, 2, 3, 4, 5, 6, 7}
+# set2 = {1, 20, 30, 45, 55, 60, 75}
+# set3 = {1, 2, 4, 56, 7, 3, 5, 100,}
+# print(set1.issuperset(set2))
+# print(set1.issubset(set3))
+# print(set1.__len__())
+
+
+
